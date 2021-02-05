1c7b5fc6d8108fe570dca357c51739402d06e2fa
Putra Widitama <putrawiditama96@gmail.com>
2021-02-05 09:52:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, perferendis.</p>
+    <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+    </ul>
 </body>
 </html>
